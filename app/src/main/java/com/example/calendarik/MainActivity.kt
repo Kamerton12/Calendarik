1acd1cedeb42ef6ca53ccd03ef1038c5d7d39282
Max <s312@tut.by>
2019-04-03 01:36:44 +0300

    init commit

---
+++ app/src/main/java/com/example/calendarik/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.example.calendarik
+
+import android.animation.Animator
+import android.animation.LayoutTransition
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import android.graphics.drawable.AnimationDrawable
+import android.view.animation.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    var key = ""
+    private val password = "1234"
+    var maxKeyLength = password.length
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val transition = LayoutTransition()
+        transition.setStartDelay(LayoutTransition.APPEARING, 0)
+        transition.setStartDelay(LayoutTransition.CHANGE_DISAPPEARING, 0)
+
+        transition.setInterpolator(LayoutTransition.CHANGE_APPEARING, DecelerateInterpolator())
+        transition.setInterpolator(LayoutTransition.CHANGE_DISAPPEARING, AccelerateInterpolator())
+
+        val animDown = ObjectAnimator.ofPropertyValuesHolder(null as View?, PropertyValuesHolder.ofFloat("scaleX", 1f, 0f), PropertyValuesHolder.ofFloat("scaleY", 1f, 0f))
+        animDown.duration = 300
+        animDown.interpolator = LinearInterpolator()
+
+        val animUp = ObjectAnimator.ofPropertyValuesHolder(null as View?, PropertyValuesHolder.ofFloat("scaleX", 0f, 1f), PropertyValuesHolder.ofFloat("scaleY", 0f, 1f))
+        animUp.duration = 300
+        animUp.interpolator = LinearInterpolator()
+
+        transition.setAnimator(LayoutTransition.APPEARING, animUp)
+        transition.setAnimator(LayoutTransition.DISAPPEARING, animDown)
+
+        pswd.layoutTransition = transition
+
+        button_backspace.setOnLongClickListener {
+            key = ""
+            textView.text = key
+            for(i in pswd.childCount-1 downTo 1)
+                pswd.removeViewAt(i)
+            return@setOnLongClickListener true
+        }
+    }
+
+    fun buttonClick(v: View){
+        if(v.tag == "back") {
+            if(key.isNotEmpty()) {
+                key = key.substring(0, key.length - 1)
+                pswd.removeViewAt(pswd.childCount - 1)
+            }
+        } else {
+            if(key.length < maxKeyLength){
+                val view = LayoutInflater.from(this).inflate(R.layout.pass_view, pswd, false)
+                pswd.addView(view)
+                key += v.tag
+            }
+        }
+        textView.text = key
+        tryPassword()
+    }
+
+    private fun tryPassword(){
+        Log.i("ggg", "$password $key")
+        if(password.length == key.length){
+            Log.i("ggg", "length")
+            if(password == key){
+                Log.i("ggg", "all")
+            } else {
+                val off = 20f
+
+                val dur = 50L
+
+                val startAnim = TranslateAnimation(0f, -off, 0f, 0f)
+                startAnim.interpolator = DecelerateInterpolator()
+                startAnim.duration = dur / 2
+
+                startAnim.setAnimationListener(object: Animation.AnimationListener{
+                    override fun onAnimationRepeat(animation: Animation?) {
+                    }
+
+                    override fun onAnimationEnd(animation: Animation?) {
+                        val anim = TranslateAnimation(-off, off, 0f, 0f)
+                        anim.interpolator = AccelerateDecelerateInterpolator()
+                        anim.duration = dur
+                        anim.repeatMode = Animation.REVERSE
+                        anim.repeatCount = 5
+
+                        shake.startAnimation(anim)
+
+                        anim.setAnimationListener(object: Animation.AnimationListener{
+                            override fun onAnimationRepeat(animation: Animation?) {
+                            }
+
+                            override fun onAnimationEnd(animation: Animation?) {
+                                val endAnim = TranslateAnimation(-off, 0f, 0f, 0f)
+                                endAnim.interpolator = AccelerateInterpolator()
+                                endAnim.duration = dur / 2
+
+                                shake.startAnimation(endAnim)
+                            }
+
+                            override fun onAnimationStart(animation: Animation?) {
+                            }
+
+                        })
+                    }
+                    override fun onAnimationStart(animation: Animation?) {
+                    }
+                })
+                shake.startAnimation(startAnim)
+            }
+        }
+    }
+}
